fe5f455455445199fac90dabdf9390138384281d
evilpug <36086890@gmail.com>
2024-01-13 14:17:53 +0300

    feat(new_users): added script to find new and deleted users

---
+++ src/new_old_users.py
@@ -0,0 +1,12 @@
+from utils import ChatTools, signin
+
+import config
+
+if __name__ == '__main__':
+    dog_client = signin(config.BOT_API_ID, config.BOT_API_HASH)
+
+    with dog_client:
+
+        ct = ChatTools(dog_client, config.MAIN_CHAT_ID)
+        ct.find_db_users_not_in_chat()
+        ct.find_chat_users_not_in_db()
